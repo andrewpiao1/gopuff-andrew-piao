abbe843d460d6d888969fa2c4b85910854eb8ee7
andrewpiao1 <andrew.piao1@gmail.com>
2020-05-31 17:29:39 -0400

    set request to state

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,9 +5,12 @@ export default class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initCart: {}
-      
+      products: [],
+      postalCode: '',
+      paymentMethod: '',
+      userData: {}
     }
+    this.getInitCart = this.getInitCart.bind(this)
   }
 
   componentDidMount(){
@@ -18,18 +21,23 @@ export default class Cart extends Component {
     axios.get('https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json?fbclid=IwAR2dEMZK5yqhuNDgeC-_QpEZ5PazuwO6Fn0xaxKeUFt6ijjOg0Gafa5DoQc')
       .then(res => {
         console.log('res:', res.data.cart.products)
-        this.setState({ initCart: res.data.cart })
+        this.setState({ 
+          products: res.data.cart.products,
+          postalCode: res.data.postal_code,
+          paymentMethod: res.data.payment_method,
+          userData: res.data.user
+        })
       })
       .catch(e => console.log(e))
     }
 
-
   render() {
-    // const { initCart } = this.state
+    // const { products, postalCode, paymentMethod, userData } = this.state
+    console.log('state', this.state)
     return (
       <div>
-        {/* {initCart} */}
-        <button onClick={this.getInitCart}>CLICK ME</button>
+        {}
+        <button onClick={() => this.getInitCart()}>CLICK ME</button>
         
       </div>
     )
